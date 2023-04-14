f29d90f57e5703e6100c5ffd489b621e5a66fb2f
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2023-04-14 23:40:59 +0100

    fix this

--- run.py
+++ run.py
@@ -5,4 +5,5 @@ import uvicorn
 from app.api import app
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    print('\33[36mAPI Running.\33[0m')
+    uvicorn.run(app, host='0.0.0.0', port=8000, log_level="critical")
